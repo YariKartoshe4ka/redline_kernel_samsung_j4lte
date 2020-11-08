5d8a933b25d3be746c38c7ea5c4ba98822caa037
Hendra Manudinata <73928476+redline-dev@users.noreply.github.com>
2020-11-08 12:43:01 +0700

    rtl8188eu: replace __attribute__((fallthrough)) with /* fall through */

--- drivers/staging/rtl8188eu/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8188eu/core/rtw_mlme_ext.c
@@ -1831,6 +1831,7 @@ void mgt_dispatcher(_adapter *padapter, union recv_frame *precv_frame)
 			ptable->func = &OnAuth;
 		else
 			ptable->func = &OnAuthClient;
+	/* fall through */
 
 	case WIFI_ASSOCREQ:
 	case WIFI_REASSOCREQ:
